79650fd3c34d06b83e05ee543ac857c687348fa8
Tristan Lotivio <tclotivio@outlook.com>
2020-12-29 23:47:42 -0500

    basic update to include firebase stuff, and other absolute indexing

--- project-name/src/index.js
+++ project-name/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from 'components/ScrollToTop'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
+    <ScrollToTop/>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
